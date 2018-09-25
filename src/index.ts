cfe3d793c9ae13dee2c4e1422bff6e29b7aec705
Chris Wendt <chrismwendt@gmail.com>
2018-09-24 21:44:44 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,123 +1,117 @@
-import { GQLService } from "@playlyfe/gql";
-import * as fs from "fs";
-import * as rpc from "vscode-ws-jsonrpc";
+import { GQLService } from '@playlyfe/gql'
+import * as fs from 'fs'
+import * as rpc from 'vscode-ws-jsonrpc'
+import * as ws from 'ws'
 
-import * as ws from "ws";
-
-const wss = new ws.Server({
-  port: 1234
-});
-
-console.log("listening");
-wss.on("connection", ws => {
-  // ws.on("close", console.log);
-  // ws.on("error", console.log);
-  ws.on("message", console.log);
-  // ws.on("open", console.log);
-  console.log("got conn");
-  const socket: rpc.IWebSocket = {
-    send: content =>
-      ws.send(content, error => {
-        if (error) {
-          throw error;
-        }
-      }),
-    onMessage: cb => {
-      console.log("msg", cb);
-      return ws.on("message", cb);
-    },
-    onError: cb => ws.on("error", cb),
-    onClose: cb => ws.on("close", cb),
-    dispose: () => ws.close()
-  };
-  // launch the server when the web socket is opened
-  if (ws.readyState === ws.OPEN) {
-    launch(socket);
-  } else {
-    ws.on("open", () => launch(socket));
-  }
-});
-
-function launch(socket: rpc.IWebSocket) {
-  console.log("launch");
-
-  const reader = new rpc.WebSocketMessageReader(socket);
-  const writer = new rpc.WebSocketMessageWriter(socket);
-  const logger = new rpc.ConsoleLogger();
-  const connection = rpc.createMessageConnection(reader, writer, logger);
-  connection.onRequest(
-    new rpc.RequestType2("hov"),
-    (
-      doc: { uri: string; languageId: string; text: string },
-      pos: { line: number; character: number }
-    ) =>
-      new Promise(resolve => {
-        fs.writeFileSync("schema.gql", doc.text);
-
-        const s = new GQLService({
-          onInit: () => {
-            const d = s.getInfo({
-              sourceText: fs.readFileSync("schema.gql").toString(),
-              sourcePath: "schema.gql",
-              position: {
-                line: pos.line + 1,
-                column: pos.character + 1
-              }
-            });
-            console.log("info", d);
-            resolve(d);
-          }
-        });
-      })
-  );
-
-  connection.onRequest(
-    new rpc.RequestType2("def"),
-    (
-      doc: { uri: string; languageId: string; text: string },
-      pos: { line: number; character: number }
-    ) =>
-      new Promise(resolve => {
-        fs.writeFileSync("schema.gql", doc.text);
-
-        const s = new GQLService({
-          onInit: () => {
-            const d = s.getDef({
-              sourceText: fs.readFileSync("schema.gql").toString(),
-              sourcePath: "schema.gql",
-              position: {
-                line: pos.line + 1,
-                column: pos.character + 1
-              }
-            });
-            console.log("def", d);
-            resolve(
-              d && {
-                start: {
-                  line: d.start.line - 1,
-                  character: d.start.column - 1
-                },
-                end: { line: d.end.line - 1, character: d.end.column - 1 }
-              }
-            );
-          }
-        });
-      })
-  );
-
-  connection.listen();
+interface TextDocument {
+    uri: string
+    text: string
 }
 
-// const s = new GQLService({
-//   onInit: () => {
-//     const d = s.getDef({
-//       sourceText: fs.readFileSync("schema.gql").toString(),
-//       sourcePath: "schema.gql",
-//       position: {
-//         line: 6,
-//         column: 10
-//       }
-//     });
-//     console.log("huh", d);
-//   }
-// });
+interface Position {
+    line: number
+    character: number
+}
+
+interface Location {
+    uri: string
+    start: Position
+    end: Position
+}
+
+function listen({ port, onConnection }: { port: number; onConnection: (socket: rpc.IWebSocket) => void }) {
+    const wss = new ws.Server({ port })
+
+    console.log('Listening for WebSocket connections on port', port, '...')
+
+    wss.on('connection', ws => {
+        console.log('Client connected')
+
+        const socket: rpc.IWebSocket = {
+            send: content =>
+                ws.send(content, error => {
+                    if (error) {
+                        throw error
+                    }
+                }),
+            onMessage: cb => ws.on('message', cb),
+            onError: cb => ws.on('error', cb),
+            onClose: cb => ws.on('close', cb),
+            dispose: () => ws.close(),
+        }
+
+        if (ws.readyState === ws.OPEN) {
+            onConnection(socket)
+        } else {
+            ws.on('open', () => onConnection(socket))
+        }
+    })
+}
+
+function serveClient(socket: rpc.IWebSocket) {
+    const connection = rpc.createMessageConnection(
+        new rpc.WebSocketMessageReader(socket),
+        new rpc.WebSocketMessageWriter(socket),
+        rpc.NullLogger
+    )
+
+    function makeGQLService(text: string): Promise<{ path: string; gqlService: GQLService }> {
+        return new Promise(resolve => {
+            const path = 'schema.gql'
+            fs.writeFileSync(path, text)
+
+            const gqlService = new GQLService({
+                onInit: () => resolve({ path, gqlService }),
+            })
+        })
+    }
+
+    connection.onRequest(new rpc.RequestType2('hover'), (doc: TextDocument, pos: Position) =>
+        makeGQLService(doc.text).then(({ path, gqlService }) =>
+            gqlService.getInfo({
+                sourceText: doc.text,
+                sourcePath: path,
+                position: {
+                    line: pos.line + 1,
+                    column: pos.character + 1,
+                },
+            })
+        )
+    )
+
+    connection.onRequest(
+        new rpc.RequestType2('definition'),
+        (doc: TextDocument, pos: Position): Promise<Location> =>
+            makeGQLService(doc.text).then(({ path, gqlService }) => {
+                const definition = gqlService.getDef({
+                    sourceText: doc.text,
+                    sourcePath: path,
+                    position: {
+                        line: pos.line + 1,
+                        column: pos.character + 1,
+                    },
+                })
+                return (
+                    definition && {
+                        uri: doc.uri,
+                        start: {
+                            line: definition.start.line - 1,
+                            character: definition.start.column - 1,
+                        },
+                        end: { line: definition.end.line - 1, character: definition.end.column - 1 },
+                    }
+                )
+            })
+    )
+
+    connection.listen()
+}
+
+function main() {
+    listen({
+        port: 1234,
+        onConnection: serveClient,
+    })
+}
+
+main()
